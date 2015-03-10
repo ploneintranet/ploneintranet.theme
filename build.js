c4d3556492b64ca413b0793be20588e9ba04bc9b
JC Brand <jc@opkode.com>
2015-03-10 09:03:39 +0100

    Add path for pat-base

--- build.js
+++ build.js
@@ -44,6 +44,7 @@
 
         // Core
         "pat-utils": "bower_components/patternslib/src/core/utils",
+        "pat-base": "bower_components/patternslib/src/core/base",
         "pat-compat": "bower_components/patternslib/src/core/compat",
         "pat-jquery-ext": "bower_components/patternslib/src/core/jquery-ext",
         "pat-logger": "bower_components/patternslib/src/core/logger",
